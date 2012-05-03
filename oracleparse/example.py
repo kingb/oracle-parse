582e4bfb6d6f71b98187f163887898117078c85b
Brandon W. King <kingb@berkeley.edu>
2012-05-03 13:43:38 -0700

    The start of the Oracle.

---
+++ oracleparse/example.py
@@ -0,0 +1,17 @@
+'''
+Created on May 3, 2012
+
+@authors: Brandon King & Omar Rehmane
+'''
+
+class ExampleField(object):
+    '''
+    ExampleField: Magic
+    '''
+
+    def __init__(self, example):
+        '''
+        Constructor
+        '''
+        self.example = example
+
