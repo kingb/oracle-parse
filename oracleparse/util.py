6f2b8b148e79f53e5c2b7d46ae67ef311da412e5
Brandon W. King <kingb@berkeley.edu>
2012-05-03 16:35:35 -0700

    node_to_absolute_XPATH(node) function implemented. =)

--- oracleparse/util.py
+++ oracleparse/util.py
@@ -11,3 +11,25 @@ def url_to_DOM(url):
     html = www.read()
 
     return soupparser.fromstring(html)
+
+
+def node_to_absolute_XPATH(node):
+    """
+    Returns an absolute XPATH for a node
+    """
+    def helper(node, current_list):
+        if current_list is None:
+            current_list = []
+
+        parent = node.getparent()
+        if parent is None or parent.tag.lower() == 'html':
+            current_list.append(node.tag)
+            return current_list
+
+        current_list.append(node.tag)
+        return helper(parent, current_list)
+
+    xpath = helper(node, [])
+    xpath.reverse()
+    xpath = '/'.join(xpath)
+    return xpath
