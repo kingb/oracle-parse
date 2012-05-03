dd3f27e48650e544d7f364db719a76ebc4a1ddd1
Omar Rehmane <orehmane@gmail.com>
2012-05-03 13:56:01 -0700

    Added .hgignore and class headers for ExampleRecord and ExampleCollection.

--- oracleparse/example.py
+++ oracleparse/example.py
@@ -6,7 +6,7 @@ Created on May 3, 2012
 
 class ExampleField(object):
     '''
-    ExampleField: Magic
+    ExampleField: This represents a column header (a "cell") in the imaginary spreadsheet.
     '''
 
     def __init__(self, example):
@@ -15,3 +15,16 @@ class ExampleField(object):
         '''
         self.example = example
 
+class ExampleRecord(object):
+    '''
+    ExampleRecord: This represents a set of fields (a "row") in the imaginary spreadsheet.
+    '''
+    def __init__(self):
+        pass
+    
+class ExampleCollection(object):
+    '''
+    ExampleCollection: This represents the sum total of the scraped data (the entire spreadsheet, as it were) in the imaginary spreadsheet.
+    '''
+    def __init__(self):
+        pass
