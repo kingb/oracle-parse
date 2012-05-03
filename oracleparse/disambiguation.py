6f3a1c1ab34fa956ea241290d43e359c70164002
Omar Rehmane <orehmane@gmail.com>
2012-05-03 16:45:13 -0700

    Added the example_to_node function to example.py and added disambiguation.py, which will contain the disambiguation functions.

---
+++ oracleparse/disambiguation.py
@@ -0,0 +1,5 @@
+def take_last(nodes):
+    """
+    Blindly returns the last node.
+    """
+    return nodes[-1]
--- oracleparse/example.py
+++ oracleparse/example.py
@@ -1,55 +1,67 @@
-'''
+"""
 Created on May 3, 2012
 
 @authors: Brandon King & Omar Rehmane
-'''
-from urllib2 import urlopen
+"""
+from util import url_to_DOM
+from disambiguation import take_last
 
 class ExampleField(object):
-    '''
+    """
     ExampleField: This represents a column header (a "cell") in the imaginary spreadsheet.
-    '''
+    """
 
     def __init__(self, name, example):
-        '''
+        """
         name - The name of the field.
         example - A string representation of an individual field from a webpage. 
-        '''
+        """
         self.name = name
         self.example = example
 
 class ExampleRecord(object):
-    '''
+    """
     ExampleRecord: This represents a set of fields (a "row") in the imaginary spreadsheet.
-    '''
+    """
     
     def __init__(self, fields=None):
-        '''
+        """
         fields - A list of associated ExampleFields.
-        '''
+        """
         if fields:
             self.fields = fields
         else:
             self.fields = []
     
 class ExampleCollection(object):
-    '''
+    """
     ExampleCollection: This represents the sum total of the scraped data (the entire spreadsheet, as it were) in the imaginary spreadsheet.
-    '''
+    """
     
     def __init__(self, records=None):
-        '''
+        """
         records - A list of associated ExampleRecords.
-        '''
+        """
         if records:
             self.records = records
         else:
             self.records= []
 
-def example_to_DOM(fields, page_url):
-    '''
-    This method takes in a list of ExampleFields and associates each one with a DOM node from the given page.
-    fields - A list of ExampleFields.
+def example_to_node(field, page_url, strip=False, disambiguation_method=take_last):
+    """
+    This method takes in an ExampleField and finds the target node that contains that example.
+    field - An ExampleField.
     page_url - The URL of the selected page.
-    '''
-    www = urlopen(page_url)
+    strip - If whitespace should be stripped from the end of the html and the example.
+    disambiguation_method - This should be a method that takes in a list and somehow decides which node to return.
+    """
+    root = url_to_DOM(page_url)
+    if strip:
+        target_nodes = [ node for node in root.iterdescendants() if node.text_content().strip() == field.example.strip() ]
+    else:
+        target_nodes = [ node for node in root.iterdescendants() if node.text_content() == field.example ]
+    if len(target_nodes) == 0:
+        raise ValueError('Node containing the given example not found.')
+    else:
+        return disambiguation_method(target_nodes)
+    
