2aecc0e227b249abcb4db81c43baea59c5050046
Brandon W. King <kingb@berkeley.edu>
2012-05-04 17:25:29 -0700

    Ash pointed out what we labeled as Album was actually Artist.

--- examples/itunes_top100.py
+++ examples/itunes_top100.py
@@ -2,7 +2,7 @@ from oracleparse import example
 from oracleparse import oracle
 
 field1 = example.ExampleField('song-title', 'Call Me Maybe')
-field2 = example.ExampleField('album-title', 'Gotye')
+field2 = example.ExampleField('artist', 'Gotye')
 
 record = example.ExampleRecord([field1, field2])
 
